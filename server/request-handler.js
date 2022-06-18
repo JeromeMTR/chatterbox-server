28314eba195b5736d45fd353c7e0ff7c81a2a526
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-06-18 11:17:54 -0700

    completed server test

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,94 +1,6 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
 const fs = require('fs');
-//fs.readFile( filename, encoding(charset ), (err, data) => {} )
-// fs.writeFile( path, data, options, (err) => {
-// successful callback
-// });
-
-// fs.readFile(`${__dirname}/data.json`, 'utf8', (err, data) => {
-//   if (err) {
-//     //we have to tell the client it was unsucessful reading...
-//     return;
-//   }
-//   console.log(data);
-//   assignDatabase(data);
-// });
-
 
 let requestHandler = function(request, response) {
-  let database = JSON.parse(fs.readFileSync(`${__dirname}/data.json`, 'utf8'));
-  console.log(database);
-
-
-  let statusCode;
-
-  // console.log(request.url == '/classes/message');
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-
-  //POST
-  //match url to classes/message if success, parse the data and add it to our database;
-  //and run the succesCB
-  //Else if there is no match, we call the errorcb statusCode = 404
-
-  let send;
-  console.log(request.url);
-
-  if (request.url === '/classes/messages') {
-    const method = request.method;
-    if (method === 'POST') {
-      statusCode = 201;
-      console.log('Successful post');
-      // request.data;
-      // fs.writeFile('./data.json', );
-
-      database.push(JSON.parse(request.data));
-      send = database;
-    }
-    if (method === 'GET') {
-      statusCode = 200;
-      console.log('successful get');
-      send = database;
-    }
-  } else {
-    statusCode = 404;
-    console.log('failed');
-    console.log(request);
-  }
-  //`http://127.0.0.1:3000/classes/messages`,
-
-  //GET
-  //match url to classes/message if success, get the data and send it to client from our database via succesful callback (provide data as argument)
-  //Else  if no match, we call the errorcb statusCode = 404
-
-
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // See the note below about CORS headers.
   var headers = {
     'access-control-allow-origin': '*',
     'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -96,40 +8,50 @@ let requestHandler = function(request, response) {
     'access-control-max-age': 10 // Seconds.
   };
 
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'application/json';
+  const respondCallback = function(statusCode, send) {
+    console.log('Serving request type ' + request.method + ' for url ' + request.url);
+    headers['Content-Type'] = 'application/json';
+    response.writeHead(statusCode, headers);
+    send ? response.end(send) : response.end();
+  };
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  const postCallback = function(parsedData) {
+    fs.writeFile(`${__dirname}/data.json`, JSON.stringify(parsedData), (err) => {
+      if (err) {
+        console.log('err: ' + err);
+      } else {
+        console.log('Succesful post. Finished writing to the file');
+        respondCallback(201);
+      }
+    });
+  };
 
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end(JSON.stringify(send));
+  const readFileCallback = function(parsedData) {
+    if (request.url === '/classes/messages') {
+      const method = request.method;
+      if (method === 'POST') {
+        request.on('data', (data) =>{
+          parsedData.push(JSON.parse(data));
+          postCallback(parsedData);
+        });
+      }
+      if (method === 'GET') {
+        console.log('successful get');
+        respondCallback(200, JSON.stringify(parsedData));
+      }
+    } else {
+      respondCallback(404);
+    }
+  };
+
+  fs.readFile(`${__dirname}/data.json`, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    const parsedData = JSON.parse(data);
+    readFileCallback(parsedData);
+  });
+
 };
-
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-// var defaultCorsHeaders = {
-//   'access-control-allow-origin': '*',
-//   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-//   'access-control-allow-headers': 'content-type, accept, authorization',
-//   'access-control-max-age': 10 // Seconds.
-// };
 
 module.exports.handleRequest = requestHandler;
