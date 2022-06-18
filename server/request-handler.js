dfe1a8029ec4277ba5d52e80037928483e7d358f
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-06-18 14:40:37 -0700

    asynch complete

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 
-let requestHandler = function(request, response) {
+module.exports.requestHandler = (request, response) => {
+  const {method, url} = request;
   var headers = {
     'access-control-allow-origin': '*',
     'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
     'access-control-allow-headers': 'content-type, accept, authorization',
-    'access-control-max-age': 10 // Seconds.
+    'access-control-max-age': 10
   };
 
   const respondCallback = function(statusCode, send) {
-    console.log('Serving request type ' + request.method + ' for url ' + request.url);
+    console.log('Serving request type ' + method + ' for url ' + url);
     headers['Content-Type'] = 'application/json';
     response.writeHead(statusCode, headers);
     send ? response.end(send) : response.end();
@@ -20,15 +21,13 @@ let requestHandler = function(request, response) {
       if (err) {
         console.log('err: ' + err);
       } else {
-        console.log('Succesful post. Finished writing to the file');
         respondCallback(201);
       }
     });
   };
 
   const readFileCallback = function(parsedData) {
-    if (request.url === '/classes/messages') {
-      const method = request.method;
+    if (url === '/classes/messages') {
       if (method === 'POST') {
         request.on('data', (data) =>{
           parsedData.push(JSON.parse(data));
@@ -36,7 +35,6 @@ let requestHandler = function(request, response) {
         });
       }
       if (method === 'GET') {
-        console.log('successful get');
         respondCallback(200, JSON.stringify(parsedData));
       }
     } else {
@@ -45,13 +43,7 @@ let requestHandler = function(request, response) {
   };
 
   fs.readFile(`${__dirname}/data.json`, 'utf8', (err, data) => {
-    if (err) {
-      throw err;
-    }
-    const parsedData = JSON.parse(data);
-    readFileCallback(parsedData);
+    err ? console.err(err) : readFileCallback(JSON.parse(data));
   });
 
 };
-
-module.exports.handleRequest = requestHandler;
